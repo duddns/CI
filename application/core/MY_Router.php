d81b5a2f277038b73dcac09ca7e3870fdf2082c2
duddns <duddns@duddns-desktop.(none)>
2013-01-24 18:35:21 +0900

    여러단계 서브디렉토리를 지원하기 위한 코어 확장

---
+++ application/core/MY_Router.php
@@ -0,0 +1,127 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.1.6 or newer
+ *
+ * @package		CodeIgniter
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2008 - 2011, EllisLab, Inc.
+ * @license		http://codeigniter.com/user_guide/license.html
+ * @link		http://codeigniter.com
+ * @since		Version 1.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Router Class
+ *
+ * Parses URIs and determines routing
+ *
+ * @package		CodeIgniter
+ * @subpackage	Libraries
+ * @author		ExpressionEngine Dev Team
+ * @category	Libraries
+ * @link		http://codeigniter.com/user_guide/general/routing.html
+ */
+class MY_Router extends CI_Router {
+    
+    // --------------------------------------------------------------------
+
+    /**
+	 * Validates the supplied segments.  Attempts to determine the path to
+	 * the controller.
+	 *
+	 * @access	private
+	 * @param	array
+	 * @return	array
+	 */  
+    function _validate_request($segments)
+    {
+        if (count($segments) == 0)
+        {
+            return $segments;
+        }
+        
+        // Does the requested controller exist in the root folder?
+        if (file_exists(APPPATH.'controllers/'.$this->fetch_directory().$segments[0].'.php'))
+        {
+            return $segments;
+        }
+
+        // Is the controller in a sub-folder?
+        if (is_dir(APPPATH.'controllers/'.$this->fetch_directory().$segments[0]))
+        {        
+            // Set the directory and remove it from the segment array
+            $this->_append_directory($segments[0]);
+            $segments = array_slice($segments, 1);
+            
+            if (count($segments) > 0)
+            {
+                // Does the requested controller exist in the sub-folder?
+                if ( ! file_exists(APPPATH.'controllers/'.$this->fetch_directory().$segments[0].'.php'))
+                {
+                    // 여러 단계 sub directory 를 지원하기 위한 patch
+                    return $this->_validate_request($segments);
+                }
+            }
+            else
+            {
+                // Is the method being specified in the route?
+				if (strpos($this->default_controller, '/') !== FALSE)
+				{
+					$x = explode('/', $this->default_controller);
+
+					$this->set_class($x[0]);
+					$this->set_method($x[1]);
+				}
+				else
+				{
+					$this->set_class($this->default_controller);
+					$this->set_method('index');
+				}
+            
+                // Does the default controller exist in the sub-folder?
+                if ( ! file_exists(APPPATH.'controllers/'.$this->fetch_directory().$this->default_controller.'.php'))
+                {
+                    $this->directory = '';
+                    return array();
+                }
+            
+            }
+
+            return $segments;
+        }
+        
+        
+        // If we've gotten this far it means that the URI does not correlate to a valid
+		// controller class.  We will now see if there is an override
+		if ( ! empty($this->routes['404_override']))
+		{
+			$x = explode('/', $this->routes['404_override']);
+
+			$this->set_class($x[0]);
+			$this->set_method(isset($x[1]) ? $x[1] : 'index');
+
+			return $x;
+		}
+		
+
+        // Nothing else to do at this point but show a 404
+        show_404($segments[0]);
+    }
+
+    /**
+     *  Append the directory name
+     *
+     * @access  public
+     * @param   string
+     * @return  void
+     */ 
+    function _append_directory($dir)
+    {
+        $this->directory .= $dir.'/';
+    }
+}
