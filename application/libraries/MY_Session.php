ac1e22de5aac4802d7934456911b7b17b53b1ad9
duddns <duddns@gmail.com>
2013-02-14 16:46:41 +0900

    ajax 요청일 경우에 세션이 죽는 문제 수정

--- application/libraries/MY_Session.php
+++ application/libraries/MY_Session.php
@@ -27,22 +27,45 @@
 class MY_Session extends CI_Session {
 
     /**
-     * Session Constructor
-     *
-     * The constructor runs the session routines automatically
-     * whenever the class is instantiated.
-     */
-    public function __construct($params = array())
-    {
-        // Set the super object to a local variable for use throughout the class
-        $CI =& get_instance();
-        
-        // 로봇은 세션 생성 안함
-        $CI->load->library('user_agent');
-        if ($CI->agent->is_robot()) {
-            return;
-        }
+	 * Session Constructor
+	 *
+	 * The constructor runs the session routines automatically
+	 * whenever the class is instantiated.
+	 */
+	public function __construct($params = array())
+	{
+	    /*
+	     * 로봇은 세션 생성 안하도록
+	     */
+	    
+		$CI =& get_instance();
+		
+		$CI->load->library('user_agent');
+		if (!$CI->user_agent->is_robot()) {
+		    parent::__construct();
+		}
+	}
+	
+    // --------------------------------------------------------------------
 
-        parent::__construct();
+    /**
+     * sess_update()
+     *
+     * Do not update an existing session on ajax or xajax calls
+     *
+     * @access    public
+     * @return    void
+     */
+    public function sess_update()
+    {
+        /*
+         * ajax 요청을 경우에 세션이 사라지는 문제 수정
+         */
+        
+        $CI =& get_instance();
+
+        if (!$CI->input->is_ajax_request()) {
+            parent::sess_update();
+        }
     }
 }
